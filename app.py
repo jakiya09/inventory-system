534e00ea9243a7797f031df41a40e8d1b10063a3
jakiya09 <jakiya.mirailit.ltd@gmail.com>
2026-04-12 16:58:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
-import os
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
 def db():
     conn = sqlite3.connect("database.db")
@@ -25,12 +25,76 @@ def init_db():
 with app.app_context():
     init_db()
 
+# LOGIN
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["username"] == "admin" and request.form["password"] == "1234":
+            session["user"] = "admin"
+            return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+# HOME + SEARCH
 @app.route("/")
 def home():
+    if "user" not in session:
+        return redirect("/login")
+
+    search = request.args.get("q", "")
+
     conn = db()
-    products = conn.execute("SELECT * FROM products").fetchall()
+    if search:
+        products = conn.execute(
+            "SELECT * FROM products WHERE name LIKE ?",
+            ('%' + search + '%',)
+        ).fetchall()
+    else:
+        products = conn.execute("SELECT * FROM products").fetchall()
+
     conn.close()
     return render_template("index.html", products=products)
+
+# ADD
+@app.route("/add", methods=["POST"])
+def add():
+    conn = db()
+    conn.execute("INSERT INTO products (name, qty, price) VALUES (?, ?, ?)",
+                 (request.form["name"], request.form["qty"], request.form["price"]))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+# SELL
+@app.route("/sell/<int:id>")
+def sell(id):
+    conn = db()
+    conn.execute("UPDATE products SET qty = qty - 1 WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+# RETURN
+@app.route("/return/<int:id>")
+def ret(id):
+    conn = db()
+    conn.execute("UPDATE products SET qty = qty + 1 WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+# DELETE
+@app.route("/delete/<int:id>")
+def delete(id):
+    conn = db()
+    conn.execute("DELETE FROM products WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
