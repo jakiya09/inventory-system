1e5beff384211d96f65c86ce09e7cbb60083dbfb
jakiya09 <jakiya.mirailit.ltd@gmail.com>
2026-04-12 16:40:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import sqlite3
 from flask import Flask, render_template, request, redirect
+import sqlite3
+import os
 
 app = Flask(__name__)
 
-# DB connection
 def db():
     conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
-# CREATE TABLE
 def init_db():
     conn = db()
     conn.execute("""
@@ -23,7 +22,8 @@ def init_db():
     conn.commit()
     conn.close()
 
-init_db()
+with app.app_context():
+    init_db()
 
 @app.route("/")
 def home():
@@ -31,45 +31,6 @@ def home():
     products = conn.execute("SELECT * FROM products").fetchall()
     conn.close()
     return render_template("index.html", products=products)
-
-@app.route("/add", methods=["POST"])
-def add():
-    name = request.form["name"]
-    qty = request.form["qty"]
-    price = request.form["price"]
-
-    conn = db()
-    conn.execute(
-        "INSERT INTO products (name, qty, price) VALUES (?, ?, ?)",
-        (name, qty, price)
-    )
-    conn.commit()
-    conn.close()
-    return redirect("/")
-
-@app.route("/sell/<int:id>")
-def sell(id):
-    conn = db()
-    conn.execute("UPDATE products SET qty = qty - 1 WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-    return redirect("/")
-
-@app.route("/return/<int:id>")
-def ret(id):
-    conn = db()
-    conn.execute("UPDATE products SET qty = qty + 1 WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-    return redirect("/")
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    conn = db()
-    conn.execute("DELETE FROM products WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
-    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
